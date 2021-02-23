64d09b682576fb78d240405c03ad56d083f1623b
LibertySky <hello@LibertySkygraphics.com>
2021-02-23 17:29:08 -0400

    adding vuex

--- src/App.vue
+++ src/App.vue
@@ -8,23 +8,36 @@ import TheHeader from './components/layout/TheHeader.vue';
 
 export default {
   components: {
-    TheHeader
-  }  
-}
+    TheHeader,
+  },
+};
 </script>
 
 <style>
-@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
 
 * {
   box-sizing: border-box;
 }
 
 html {
-  font-family: "Roboto", sans-serif;
+  font-family: 'Roboto', sans-serif;
 }
 
 body {
   margin: 0;
+}
+.my-reviews {
+  color: red !important;
+}
+.glsr-button {
+  background-color: #d91206 !important;
+  color: #f4f4f4 !important;
+  cursor: pointer !important;
+  transition: all 0.3s !important;
+}
+.glsr-button:hover {
+  background-color: #450000 !important;
+  color: #fff !important;
 }
 </style>
